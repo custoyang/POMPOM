73a7064a6244cf3ecbffbadc21991e472e926915
Custo Yang <cyanghua@uci.edu>
2024-05-10 05:02:44 -0700

    Added several html templates for different directories

--- flask-server/website/__init__.py
+++ flask-server/website/__init__.py
@@ -1,15 +1,43 @@
 from flask import Flask
+from flask_sqlalchemy import SQLAlchemy
+from os import path
+from flask_login import LoginManager
+
+db = SQLAlchemy()
+DB_NAME = "database.db"
+
 
 def create_app():
+    # setup
     app = Flask(__name__)
     app.config['SECRET_KEY'] = 'epsilonbeta'
+    app.config['SQLALCHEMY_DATABASE_URI'] = f'sqlite:///{DB_NAME}'
+    db.init_app(app)
 
     # blueprints
     from .views import views
     from .auth import auth
 
+    # directory
     app.register_blueprint(views, url_prefix='/')
     app.register_blueprint(auth, url_prefix='/')
 
+    from .models import User, Pill
+
+    create_database(app)
+
+    login_manager = LoginManager()
+    login_manager.login_view = 'auth.login'
+    login_manager.init_app(app)
+
+    @login_manager.user_loader
+    def load_user(id):
+        return User.query.get(int(id))
 
     return app
+
+def create_database(app):
+    if not path.exists('website/' + DB_NAME):
+        with app.app_context():  # Set up the application context
+            db.create_all()      # Create the tables
+            print('Created Database!')
