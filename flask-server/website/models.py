154d03122636f16abb5b8bfde78e089a95dd91f6
Maggie Zhang <maggiegzhang@outlook.com>
2024-05-12 21:27:10 -0700

    tweaked additional ui, began translating front-end to database, adding calendar functionality (custo contributions through liveshare)

--- flask-server/website/models.py
+++ flask-server/website/models.py
@@ -8,10 +8,10 @@ class Pill(db.Model):
     amount = db.Column(db.Integer, nullable=False)
     name = db.Column(db.String(100), nullable=False)
     alt_name = db.Column(db.String(100))
-    dispense_time = db.Column(db.Time)  # Assuming dispense time is a time field
-    frequency = db.Column(db.String(20), nullable=False)  # Frequency dropdown options like daily, biweekly, etc.
-    start_date = db.Column(db.Date, nullable=False)  # Start date of dispensing
-    end_date = db.Column(db.Date)  # End date of dispensing (can be nullable)
+    dispense_time = db.Column(db.Time)
+    frequency = db.Column(db.String(20), nullable=False) 
+    start_date = db.Column(db.Date, nullable=False)
+    end_date = db.Column(db.Date)
     user_id = db.Column(db.Integer, db.ForeignKey('user.id'), nullable=False)
 
 class User(db.Model, UserMixin):
