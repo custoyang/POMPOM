68ee63cf3b5537d943fa10cf2c5c5535534af0cb
Shana Chao <shanaxchao@gmail.com>
2024-05-14 21:30:24 -0700

    added UI functionality to edit profile, modified compartment class

--- rpi_movement/compartment.py
+++ rpi_movement/compartment.py
@@ -1,18 +1,23 @@
 class Compartment:
+
   def __init__(self, channel, pillCount, pill, frequency):
     self.channel = channel
     self.pillCount = pillCount
     self.pill = pill
-    self.frequency = frequency
- 
+    self.frequency = frequency # in hours
+
+  # Sets pill count
   def setPills(self, added):
     self.pillCount = added
 
+  # Adds pills to pill count
   def addPills(self, added):
     pillCount += added
 
+  # Returns the number of pills in the compartment
   def getPills(self):
     return self.pillCount
   
+  # Returns the channel of the motor in the compartment
   def getChannel(self):
     return self.channel
