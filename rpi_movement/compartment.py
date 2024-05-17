fb8112c3ba929846632ab94dbfae4ec617c8da75
Lance Vu <lancev@uci.edu>
2024-05-17 01:07:11 -0700

    changed from adafruit library to gpiozero library for better servo functionality, added calibration method to compartment class in order to check for correct pill hole

--- rpi_movement/compartment.py
+++ rpi_movement/compartment.py
@@ -1,23 +1,70 @@
-class Compartment:
+from gpiozero import Servo
+import time
+import servo_movement
 
-  def __init__(self, channel, pillCount, pill, frequency):
-    self.channel = channel
-    self.pillCount = pillCount
-    self.pill = pill
-    self.frequency = frequency # in hours
+class Compartment:
+    def __init__(self, pin, pillCount, pill, frequency, size):
+        self.pin = Servo(pin)
+        self.pillCount = pillCount
+        self.pill = pill
+        self.frequency = frequency # in hours
+        self.size = size
+        self.delay = 3.3
+        self.speed = 0.025
+    
+    def calibration(self, new_size):
+        servo = Servo(self.pin)
+        if self.size == new_size:
+            return
+        elif self.size == 'XL' and new_size == 'L':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.75)
+        elif self.size == 'XL' and new_size == 'M':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.5)
+        elif self.size == 'XL' and new_size == 'S':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.25)
+        elif self.size == 'L' and new_size == 'XL':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.25)
+        elif self.size == 'L' and new_size == 'M':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.75)
+        elif self.size == 'L' and new_size == 'S':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.5)
+        elif self.size == 'M' and new_size == 'XL':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.5)
+        elif self.size == 'M' and new_size == 'L':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.75)
+        elif self.size == 'M' and new_size == 'S':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.25)
+        elif self.size == 'S' and new_size == 'XL':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.25)
+        elif self.size == 'S' and new_size == 'L':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.5)
+        elif self.size == 'S' and new_size == 'M':
+            servo.value = self.speed
+            time.sleep(self.delay * 0.75)
 
   # Sets pill count
-  def setPills(self, added):
-    self.pillCount = added
+    def setPills(self, added):
+        self.pillCount = added
 
   # Adds pills to pill count
-  def addPills(self, added):
-    pillCount += added
+    def addPills(self, added):
+        pillCount += added
 
   # Returns the number of pills in the compartment
-  def getPills(self):
-    return self.pillCount
+    def getPills(self):
+        return self.pillCount
   
   # Returns the channel of the motor in the compartment
-  def getChannel(self):
-    return self.channel
+    def getChannel(self):
+        return self.channel
--- rpi_movement/servo_movement.py
+++ rpi_movement/servo_movement.py
@@ -1,59 +1,16 @@
+from gpiozero import Servo
 import time
-from adafruit_servokit import ServoKit
 
-# Setup PCA9685 module
-kit = ServoKit(channels=16)
+delay = 3.3
+speed = 0.025
+servo1 = Servo(12)
+servo2 = Servo(13)
+servo3 = Servo(18)
+servo4 = Servo(19)
 
-# Define channels for the servos
-channel_4 = 4
-channel_6 = 6
 
-def control_servo(channel, action):
-    """Control the servo's movement."""
-    if action == "stop":
-        # Stop the servo by sending a neutral pulse (about 90 degrees in standard servos)
-        kit.servo[channel].angle = 90
-    elif action == "clockwise":
-        # Rotate clockwise (we might adjust the value slightly less than 90)
-        kit.servo[channel].angle = 85
-    elif action == "counterclockwise":
-        # Rotate counterclockwise (we might adjust the value slightly more than 90)
-        kit.servo[channel].angle = 95
-
-    print(f"Servo on channel {channel} set to {action}.")
-
-def main():
-    try:
-        # Rotate servo on channel 4 clockwise
-        control_servo(channel_4, "clockwise")
-        time.sleep(2)  # Spin for 2 seconds
-
-        # Stop servo on channel 4
-        control_servo(channel_4, "stop")
-        time.sleep(1)  # Stop for 1 second
-
-        # Rotate servo on channel 4 counterclockwise
-        control_servo(channel_4, "counterclockwise")
-        time.sleep(2)  # Spin for 2 seconds
-
-        # Stop servo on channel 4
-        control_servo(channel_4, "stop")
-        time.sleep(1)  # Stop for 1 second
-
-        # Repeat the same for channel 6
-        control_servo(channel_6, "clockwise")
-        time.sleep(2)
-        control_servo(channel_6, "stop")
-        time.sleep(1)
-        control_servo(channel_6, "counterclockwise")
-        time.sleep(2)
-        control_servo(channel_6, "stop")
-
-        print("Demo complete. Both servos returned to stop position.")
-
-    except KeyboardInterrupt as e:
-        print(f"An error occurred: {e}")
-        control_servo(channel_4, 'stop')
-
-if __name__ == "__main__":
-    main()
+def dispense_pill(servo):
+    servo.value = speed
+    time.sleep(delay)
+    servo.value = None
+    time.sleep(delay)
