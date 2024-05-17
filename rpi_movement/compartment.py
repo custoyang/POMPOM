df2159a2ab23b010ae2bbf9b5b9059327b2eefa2
Lance Vu <lancev@uci.edu>
2024-05-17 03:25:25 -0700

    calibration settings - low, med, high times

--- rpi_movement/compartment.py
+++ rpi_movement/compartment.py
@@ -8,50 +8,53 @@ class Compartment:
         self.pill = pill
         self.frequency = frequency # in hours
         self.size = size
-        self.delay = 2.3
+        self.delay = 2.7
         self.speed = 0.025
         self.servo = Servo(self.pin)
         self.servo.value = None
     
     def calibration(self, new_size):
+        low = 0.16
+        med = 0.45
+        high = 0.715
         if self.size == new_size:
             return
         elif self.size == 'XL' and new_size == 'L':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.75)
+            time.sleep(self.delay * low)
         elif self.size == 'XL' and new_size == 'M':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.5)
+            time.sleep(self.delay * med)
         elif self.size == 'XL' and new_size == 'S':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.25)
+            time.sleep(self.delay * high)
         elif self.size == 'L' and new_size == 'XL':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.25)
+            time.sleep(self.delay * low)
         elif self.size == 'L' and new_size == 'M':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.75)
+            time.sleep(self.delay * high)
         elif self.size == 'L' and new_size == 'S':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.5)
+            time.sleep(self.delay * med)
         elif self.size == 'M' and new_size == 'XL':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.5)
+            time.sleep(self.delay * med)
         elif self.size == 'M' and new_size == 'L':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.75)
+            time.sleep(self.delay * high)
         elif self.size == 'M' and new_size == 'S':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.25)
+            time.sleep(self.delay * low)
         elif self.size == 'S' and new_size == 'XL':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.25)
+            time.sleep(self.delay * low)
         elif self.size == 'S' and new_size == 'L':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.5)
+            time.sleep(self.delay * med)
         elif self.size == 'S' and new_size == 'M':
             self.servo.value = self.speed
-            time.sleep(self.delay * 0.75)
+            time.sleep(self.delay * high)
         self.servo.value = None
 
   # Sets pill count
