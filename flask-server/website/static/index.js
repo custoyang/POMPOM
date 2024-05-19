594de4cc26206f1bec3707895aec8f4f1cea3ef6
custoyang <72181969+custoyang@users.noreply.github.com>
2024-05-19 15:46:52 -0700

    Delete flask-server directory

--- flask-server/website/static/index.js
+++
@@ -1,8 +0,0 @@
-function deletePill(pillId) {
-    fetch('/delete-pill', {
-        method: 'POST',
-        body: JSON.stringify({ pillId: pillId }),
-    }).then((_res) => {
-        window.location.href = "/";
-    });
-}
